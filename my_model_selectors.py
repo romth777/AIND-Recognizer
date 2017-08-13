909927750986f06f45d1a3bc830a6cb5085ce6b1
romth777 <roomtheater@gmail.com>
2017-08-13 12:47:50 +0900

    add comments

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -74,21 +74,32 @@ class SelectorBIC(ModelSelector):
 
         :return: GaussianHMM object
         """
+        # Check if the warning
         warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+        # initial parameters
         num_feature = self.X.shape[1]
         best_score = float('inf')
         best_model = None
+
+        # search for min to max
         for num_components in range(self.min_n_components,self.max_n_components):
             try:
+                # set up HMM
                 model = GaussianHMM(n_components=num_components, n_iter=1000).fit(self.X, self.lengths)
+
+                # calculate BIC score
                 logL = model.score(self.X, self.lengths)
                 p = num_components * (num_components - 1) + 2 * num_components * num_feature
                 score = -2 * logL + p * np.log(len(self.X))
             except:
                 score = float("inf")
+
+            # update best
             if score < best_score:
                 best_score = score
                 best_model = model
+
         return best_model
 
 class SelectorDIC(ModelSelector):
@@ -101,19 +112,27 @@ class SelectorDIC(ModelSelector):
     '''
 
     def select(self):
+        # Check if the warning
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
+        # initial parameters
         best_hmm_model = None
         best_score = float("-inf")
 
+        # search for min to max
         for nb_components in range(self.min_n_components, self.max_n_components + 1):
             try:
+                # set up HMM
                 hmm_model = GaussianHMM(n_components=nb_components, covariance_type="diag", n_iter=1000,
                                         random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+
+                # calculate log loss
                 logL = hmm_model.score(self.X, self.lengths)
             except:
                 best_score = float("-inf")
                 best_hmm_model = None
+
+            # calculate log loss of hole words except from this word
             logL_other = []
             for word in self.words:
                 if word != self.this_word:
@@ -122,8 +141,11 @@ class SelectorDIC(ModelSelector):
                         logL_other.append(hmm_model.score(X, lengths))
                     except:
                         logL_other.append(float("-inf"))
+
+            # calculate DIC with this score and mean of the others
             score = logL - np.mean(logL_other)
 
+            # update best score
             if score > best_score:
                 best_score = score
                 best_hmm_model = hmm_model
@@ -137,45 +159,62 @@ class SelectorCV(ModelSelector):
     '''
 
     def select(self):
+        # Check if the warning
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # model selection using CV
+        # initial parameters
         n_splits = min(3, len(self.lengths))
         max_mean_logL = -1000000
         best_hidden_state = 0
+
+        # search for min to max
         for nb_hidden_state in range(self.min_n_components, self.max_n_components+1):
-            #print('nb_hidden_state', nb_hidden_state)
             sum_log = 0
             n_splits_done = 0
             mean_logL = -1000000
+
+            # if not be splitted
             if n_splits == 1 :
                 try :
+                    # set up model
                     hmm_model = GaussianHMM(n_components=nb_hidden_state, covariance_type="diag", n_iter=1000,
                                             random_state=self.random_state, verbose=False).fit(self.X, self.lengths)
+
+                    # calculate the score
                     mean_logL = hmm_model.score(self.X, self.lengths)
                 except:
                     continue
 
+            # if be splitted
             else :
+                # set up K-Fold
                 split_method = KFold(n_splits=n_splits)
+                # for each splitted data with train and test
                 for cv_train_idx, cv_test_idx in split_method.split(self.sequences):
+                    # recombine the sequence
                     X_train, lengths_train = combine_sequences(cv_train_idx, self.sequences)
                     try:
-                        model, logL =  self.selected_model(nb_hidden_state, X_train, lengths_train)
+                        # generate the model
+                        model, logL = self.selected_model(nb_hidden_state, X_train, lengths_train)
                         sum_log += logL
                         n_splits_done += 1
                     except:
                         continue
+
+                # if split done
                 if n_splits_done != 0 :
                     mean_logL = sum_log / float(n_splits_done)
                 else:
                     mean_logL = -1000000
-            #print('mean_logL', mean_logL)
+
+            # update best
             if(mean_logL> max_mean_logL):
                 max_mean_logL = mean_logL
                 best_hidden_state = nb_hidden_state
-        #print('max_mean_logL', max_mean_logL)
+
+        # if there are no best model then choice n_constant
         if best_hidden_state == 0:
             return self.base_model(self.n_constant)
+        # otherwise choice the best one
         else:
             return self.base_model(best_hidden_state)
